9fc280348cc5b6c601e8057b3f5cdb7362db8672
Даниил Тюрин <tjurin.daniil@rambler.ru>
2022-02-12 21:22:22 +0700

    Фикс циферок

--- Str_Tab_4e/Program.cs
+++ Str_Tab_4e/Program.cs
@@ -10,16 +10,20 @@ namespace Str_Tab_4e
             public string firstName, lastName, yearsOfEducation, letterOfClass;
             public void Print()
             {
-                Console.WriteLine($"Имя: {firstName} {lastName} Года обучения: {yearsOfEducation} {letterOfClass}");
+                Console.WriteLine($"Имя: {firstName} {lastName} Года обучения: {yearsOfEducation}{letterOfClass}");
             }
 
         }
         static void Main(string[] args)
         {
-            int n = 25;
+            int n = 5;
             Random rnd = new Random();
             string[] names = new string[n];
             string[] surnames = new string[n];
+
+            int plusator8 = 0;
+            int plusator10 = 0;
+
             names[0] = "Семен";
             names[1] = "Даниил";
             names[2] = "Артем";
@@ -30,14 +34,21 @@ namespace Str_Tab_4e
             surnames[2] = "Романов";
             surnames[3] = "Тугаринов";
             surnames[4] = "Шувалов";
+
+            Console.Write("Введите число учеников:");
+            string userInputN = Console.ReadLine();
+
+            n = untilNisNotANumber(userInputN);
+
             Person[] students = new Person[n];
-            int plusator8 = 0;
-            int plusator10 = 0;
 
             for (int i = 0; i < n; i++)
             {
-                students[i].firstName = names[rnd.Next(0, 5)];
-                students[i].lastName = surnames[rnd.Next(0, 5)];
+                int randName = rnd.Next(0, 4);
+                int randSurname = rnd.Next(0, 4);
+
+                students[i].firstName = names[randName];
+                students[i].lastName = surnames[randSurname];
                 students[i].yearsOfEducation = rnd.Next(1, 12).ToString();
                 if (students[i].yearsOfEducation == "8")
                 {
@@ -50,18 +61,36 @@ namespace Str_Tab_4e
                 students[i].letterOfClass = "a";
                 students[i].Print();
             }
-            if (plusator8 > plusator10)
+
+            isEightMoreThanTenthStudents(plusator8,plusator10);
+
+
+            void isEightMoreThanTenthStudents(int plusator8, int plusator10)
             {
-                Console.WriteLine($"В восьмом классе на {plusator8-plusator10} большей людей");
+                if (plusator8 > plusator10)
+                {
+                    Console.WriteLine($"В восьмом классе на {plusator8 - plusator10} большей учеников");
+                }
+                if (plusator8 < plusator10)
+                {
+                    Console.WriteLine($"В восьмом классе на {plusator10 - plusator8} меньше учеников");
+                }
+                if (plusator8 == plusator10)
+                {
+                    Console.WriteLine($"В восьмом и десятом классе одинаковое кол-во учеников");
+                }
             }
-            if (plusator8 < plusator10)
+
+            int untilNisNotANumber(string userInputN)
             {
-                Console.WriteLine($"В восьмом классе на {plusator10-plusator8} меньше людей");
-            }
-            if (plusator8 == plusator10)
-            {
-                Console.WriteLine($"В восьмом и десятом классе одинакова людей");
+                int number;
+                while (!int.TryParse(userInputN, out number) || (number <= 0))
+                {
+                    Console.WriteLine($"Ошибка!Введено '{userInputN}' вместо положительного числа, попробуйте снова");
+                    userInputN = Console.ReadLine();
+                }
+                return number;
             }
         }
     }
-    }
+}
